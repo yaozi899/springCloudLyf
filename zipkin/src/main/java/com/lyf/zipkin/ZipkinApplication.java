e14363cd601861b536d4b4f0c01283f8b448f2b8
553848904@qq.com <555384804@qq.com>
2019-01-06 22:04:57 +0800

    update

---
+++ zipkin/src/main/java/com/lyf/zipkin/ZipkinApplication.java
@@ -0,0 +1,18 @@
+package com.lyf.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+/**
+ * @author lyf
+ */
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinApplication.class, args);
+    }
+
+}

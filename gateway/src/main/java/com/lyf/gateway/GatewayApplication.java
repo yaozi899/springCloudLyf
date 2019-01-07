2704d5a93daad1fc851d4039c506843893ac328a
553848904@qq.com <555384804@qq.com>
2019-01-07 22:05:37 +0800

    配置修改

--- gateway/src/main/java/com/lyf/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/lyf/gateway/GatewayApplication.java
@@ -1,17 +1,11 @@
 package com.lyf.gateway;
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-/**
- * 网关Demo
- *
- * @author lyf
- * @since 2019-01-03
- */
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -22,5 +16,6 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 }
+
 
 
